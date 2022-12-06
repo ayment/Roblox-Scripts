6c441a06ef2988df0854d73244b4f50f887b07c3
ayment <93206027+ayment@users.noreply.github.com>
2022-12-06 20:36:23 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 2788229376 and game.PlaceId == 9053673709 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ayment/Roblox-Scripts/main/Cheese%20Escape.lua"))() 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ayment/Roblox-Scripts/main/scripts/Cheese%20Escape.lua"))() 
 end
