c4311ef03ca27d24c0ffe0e64493813d96bda0cc
ayment <93206027+ayment@users.noreply.github.com>
2022-12-07 08:01:32 +0300

    Update gamelist.lua

--- gamelist.lua
+++ gamelist.lua
@@ -1,4 +1,5 @@
 getgenv().Games = {
     [5777099015] = "https://raw.githubusercontent.com/ayment/Roblox-Scripts/main/scripts/Cheese%20Escape.lua",
     [9053673709] = "https://raw.githubusercontent.com/ayment/Roblox-Scripts/main/scripts/Cheese%20Escape.lua"
+    [920587237] = "adopt me soon"
 }
